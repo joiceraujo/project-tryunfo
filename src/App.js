984e5402174babf63f3c20fafd398f602e7b9468
Joice Araújo <joicearaujoedu@gmail.com>
2022-04-22 15:41:57 -0300

    Renderizando o componente List no App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Form from './components/Form';
 import Card from './components/Card';
+import List from './components/List';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
         <h1>Tryunfo</h1>
         <Form />
         <Card />
+        <List />
       </div>
 
     );
